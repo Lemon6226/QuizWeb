ea363796f4ed13e369de35d10844658db4c21de1
lemon <vendula.prudilova02@gmail.com>
2025-06-23 23:32:45 +0200

    okkkk

--- pages/quizzes.tsx
+++ pages/quizzes.tsx
@@ -3,7 +3,7 @@
 import { useEffect, useState } from "react";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import "../globals.css";
+import "../app/globals.css";
 
 interface Quiz {
     id: number;
