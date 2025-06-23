25227f36f6d30d14d236cde95a674a701a1e2ed6
lemon <vendula.prudilova02@gmail.com>
2025-06-23 22:56:05 +0200

    UI fix????

--- app/quizzes.tsx
+++ app/quizzes.tsx
@@ -3,7 +3,7 @@
 import { useEffect, useState } from "react";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import "../app/globals.css";
+import "./globals.css";
 
 interface Quiz {
     id: number;
