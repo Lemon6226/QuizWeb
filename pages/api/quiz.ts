ca3dc8b71347eb739da11262c94f5cc4bcc13ae0
lemon <vendula.prudilova02@gmail.com>
2025-06-23 23:22:42 +0200

    ok

--- pages/api/quiz.ts
+++ pages/api/quiz.ts
@@ -5,7 +5,7 @@ const prisma = new PrismaClient();
 
 type QuestionInput = {
     text: string;
-    options: string[];
+    options: [string];
     answer: string;
 };
 
@@ -15,6 +15,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     }
 
     try {
+
         const fallbackUserId = 1;
 
         const { title, questions }: { title: string; questions: QuestionInput[] } = req.body;
@@ -23,27 +24,22 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
             return res.status(400).json({ message: "Title is required" });
         }
 
-        if (!questions || questions.length === 0) {
+        if (!questions) {
             return res.status(400).json({ message: "Questions are required" });
         }
 
-        const user = await prisma.user.findUnique({ where: { id: fallbackUserId } });
-        if (!user) {
-            return res.status(400).json({ message: `User with id ${fallbackUserId} not found.` });
-        }
 
         const quiz = await prisma.quiz.create({
             data: {
                 title,
-                user: {
-                    connect: { id: fallbackUserId }
-                },
+                userId:fallbackUserId,
                 questions: {
                     create: questions.map((q: QuestionInput) => ({
                         text: q.text,
                         options: q.options,
                         answer: q.answer,
-                    })),
+                    }))
+
                 },
             },
         });
